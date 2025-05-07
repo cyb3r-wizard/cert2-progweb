df91a74ba570f71a6506f97b4c41d829571a8291
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2025-05-07 15:49:30 -0400

    creacion e implementación de remindersPatch y remindersDelete

--- controllers/reminders.controller.js
+++ controllers/reminders.controller.js
@@ -1,4 +1,4 @@
-import { getSortedReminders, createReminder } from "#repositories/reminders.repo.js";
+import { getSortedReminders, createReminder,updateReminder,removeReminder } from "#repositories/reminders.repo.js";
 import { reminders } from "#index.js";
 
 
@@ -7,7 +7,7 @@ const remindersGet = async (req, res) => {
   res.status(200).send(sortedReminders);
 }
 
-const remindersPost = async (req, res ) => {
+const remindersPost = async (req, res) => {
   try{
     const { content, important} = req.body;
     const response  = await createReminder(content,important)
@@ -17,68 +17,24 @@ const remindersPost = async (req, res ) => {
   }
 }
 
-const remindersPatch = (req, res) => {
-    try {
-      const { id } = req.params;
-      const reminderIndex = reminders.findIndex((reminder)=>reminder.id === id);
-      
-  
-      if (reminderIndex === -1) {
-        return res.status(404).send();
-      }
-      let reminder = reminders[reminderIndex];
-      let { important, content } = req.body;
-      
-      if (typeof important !== "undefined") {
-        if (typeof important !== "boolean") {
-          return res.status(400).json({ message: "important debe ser booleano" });
-        }
-        reminder.important = important;
-      }
-      
-      if (typeof content !== "undefined") {
-        if (typeof content !== "string") {
-          return res.status(400).json(reminder);
-        } 
-  
-        content = content.toString();
-        let contSpace = content.trim();
-  
-        if (content.length > 120) {
-          return res.status(400).json(reminder);
-        }
-        if (contSpace.length <= 0) {
-          return res.status(400).json(reminder);
-        }
-        reminder.content = content;
-      }
-      if (typeof reminder.completed === "undefined") {
-        reminder.completed = false;
-      }
-      return res.status(200).json({
-        id: reminder.id,
-        content: reminder.content,
-        createdAt: reminder.createdAt,
-        important: reminder.important,
-        completed: reminder.completed,
-      });
-    } catch (error) {
-      return res.status(400).json({ error: `reminder router error: ${error}` });
-    }
+const remindersPatch = async (req,res) =>{
+  try{
+    const { id } = req.params;
+    const { important, content } = req.body;
+    const reminder = updateReminder(id, important,content);
+    res.status(200).json(reminder);  
+  }catch(error){
+    res.status(error.status).json({ message:error.message})
+  }
 }
 
 const remindersDelete = (req, res) => {
     try {
       const { id } = req.params;
-  
-      let index = reminders.findIndex(reminder => reminder.id === id);
-      if (index === -1) {
-        return res.status(404).send();
-      }
-      reminders.splice(index,1);
+      removeReminder(id)
       return res.status(204).send();
     } catch (error) {
-      return res.status(400).json({ error: `ERROR ${error}` });
+      res.status(error.status || 500).json({ message:error.message})
     }
 }
 
--- repositories/reminders.repo.js
+++ repositories/reminders.repo.js
@@ -1,5 +1,5 @@
 import { reminders } from "#index.js";
-import crypto, { randomBytes } from "node:crypto";
+import crypto from "node:crypto";
 
 async function getSortedReminders(){
     const sortedReminders = [...reminders].sort((a, b) => {
@@ -53,7 +53,75 @@ async function createReminder(content, important){
 
    reminders.push(reminder);
    return reminder;
-
 }
 
-export { getSortedReminders, createReminder }
+function updateReminder(id, important, content){
+    const error = new Error("");
+    const reminderIndex = reminders.findIndex(reminder => reminder.id === id);
+    
+    if(reminderIndex === -1){
+        error.message = 'No se encontró el recordatorio'
+        error.status = 404
+        throw error;
+    }   
+
+    let reminder = reminders[reminderIndex];
+
+    if (typeof important !== "undefined") {
+        if (typeof important !== "boolean") {
+            error.message = 'Important debe ser booleano'
+            error.status = 400
+            throw error;
+        }
+        reminder.important = important;
+    }
+
+    if (typeof content !== "undefined") {
+        if (typeof content !== "string") {
+            error.message = 'Important debe ser un string'
+            error.status = 400
+            throw error;
+        } 
+  
+        content = content.toString();
+        let contSpace = content.trim();
+  
+        if (content.length > 120) {
+            error.message = `El mensaje es demasiado largo (max. 120 caracteres)`
+            error.status = 400;
+            throw error
+        }
+        if (contSpace.length <= 0) {
+            error.message = `El mensaje no puede ser sólo espacios`
+            error.status = 400;
+            throw error
+        }
+        reminder.content = content;
+    }
+    if (typeof reminder.completed === "undefined") {
+        reminder.completed = false;
+    }
+
+    return {
+        id: reminder.id,
+        content: reminder.content,
+        createdAt: reminder.createdAt,
+        important: reminder.important,
+        completed: reminder.completed,
+    };
+}
+
+function removeReminder(id){
+    const error = new Error("");
+    let index = reminders.findIndex(reminder => reminder.id === id);
+    if(index === -1){
+        error.message = "recordatorio no encontrado"
+        error.status = 404
+        throw error
+    }
+    reminders.splice(index,1);
+    return true;
+}
+
+
+export { getSortedReminders, createReminder,updateReminder,removeReminder }
