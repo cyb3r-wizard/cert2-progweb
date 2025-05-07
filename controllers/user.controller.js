b9892720ca6ed1f8af149bc7538c5d495175d5c9
Mario H. Tornería Ramírez <mh.torneria@gmail.com>
2025-05-07 12:10:25 -0400

    repositorio de users completado. Iniciando repositorio de reminders

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,36 +1,13 @@
-import { users } from "#index.js";
-import crypto, { randomBytes } from "node:crypto";
-const userPost =  async (req, res) => {
-    try {
-      const { password, username } = req.body;
-      if (!username || !password) {
-        return res
-          .status(400)
-          .json({ message: "Formato incorrecto de petición" });
-      }
-      let user = users.find((user) => user.username === username);
-      if (!user) {
-        return res
-          .status(401)
-          .json({ message: "Credenciales inválidas. Usuario No Pillado" });
-      }
-      const [salt, hash] = user.password.split(":");
-      const derivedKey = await crypto.scryptSync(password, salt, 64);
-      if (derivedKey.toString("hex") !== hash) {
-        return res
-          .status(401)
-          .json({ message: "Credenciales inválidas. Password mal escrito >:(" });
-      }
-      let token = randomBytes(48).toString("hex");
-      user.token = token;
-      return res.status(200).json({
-        username: user.username,
-        name: user.name,
-        token: user.token,
-      });
-    } catch (err) {
-      return res.status(403).json({ message: "user router error: " + err });
-    }
+import { login } from "#repositories/users.repo.js";
+
+const userPost = async (req,res) => {
+  try{
+    const { password, username } = req.body;
+    let user = await login(username,password);
+    return res.status(200).json(user);
+  }catch (err) {
+    return res.status(err.status).json({ message: err.message });
   }
+}
 
   export { userPost }
--- repositories/users.repo.js
+++ repositories/users.repo.js
@@ -1,21 +1,31 @@
 import { users } from "#index.js";
 import crypto, { randomBytes } from "node:crypto";
-async function login(username,password){
-      let user = users.find((user) => user.username === username);
-      if (!user) {
-        return null;
-      }
-
-      const [salt, hash] = user.password.split(":");
-      const derivedKey = crypto.scryptSync(password, salt, 64);
-      
-      if (derivedKey.toString("hex") !== hash) {
-        return null
-      }
-      
-      let token = randomBytes(48).toString("hex");
-      user.token = token;
-      return user;
+async function login(username, password) {
+  if (!username || !password) {
+    const err = new Error("Formato de petición incorrecto");
+    err.status = 400;
+    throw err;
+  }
+  let user = users.find((user) => user.username === username);
+  if (!user) {
+    const err = new Error("Credenciales inválidas. Usuario No Pillado");
+    err.status = 401;
+    throw err;
+  }
+  const [salt, hash] = user.password.split(":");
+  const derivedKey = crypto.scryptSync(password, salt, 64);
+  if (derivedKey.toString("hex") !== hash) {
+    const err = new Error("Credenciales inválidas. Usuario No Pillado");
+    err.status = 401;
+    throw err;
+  }
+  let token = randomBytes(48).toString("hex");
+  user.token = token;
+  return {
+    username: user.username,
+    name: user.name,
+    token: user.token,
+  };
 }
 
 export { login }
